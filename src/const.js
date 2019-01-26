d5437628f5037ec82f1e05d2f2548e2230b118f9
kenjiuno <h8mastre@gmail.com>
2019-01-26 23:30:49 +0900

    Remove dataBinary.

--- src/const.js
+++ src/const.js
@@ -65,7 +65,6 @@ export default {
         '3704': 'fileNameShort',
         '3707': 'fileName',
         '3712': 'pidContentId',
-        '3701': 'dataBinary',
         // recipient specific
         '3001': 'name',
         '3003': 'email'
