133a38b8b09a6a0965ee077c8070c539499556b3
KU <ku@digitaldolphins.jp>
2020-03-19 19:56:00 +0900

    - include decompressrtf for cli.js

--- cli.js
+++ cli.js
@@ -3,6 +3,8 @@ const program = require('commander');
 const MsgReader = require('./lib/MsgReader').default;
 
 const fs = require('fs');
+const path = require('path');
+const { decompressRTF } = require('@kenjiuno/decompressrtf');
 
 program
   .command('parse <msgFilePath>')
@@ -17,6 +19,53 @@ program
         ? JSON.stringify(testMsgInfo, null, 2)
         : testMsgInfo
     );
+  });
+
+program
+  .command('rtf <msgFilePath> [saveToRtfFilePath]')
+  .description('Parse msg file and print decompressed rtf')
+  .action((msgFilePath, saveToRtfFilePath) => {
+    const msgFileBuffer = fs.readFileSync(msgFilePath)
+    const testMsg = new MsgReader(msgFileBuffer)
+    const testMsgInfo = testMsg.getFileData()
+
+    const body = Buffer.from(decompressRTF(testMsgInfo.compressedRtf))
+
+    if (typeof saveToRtfFilePath === "string" && saveToRtfFilePath.length >= 1) {
+      fs.writeFileSync(saveToRtfFilePath, body);
+    }
+    else {
+      console.log(body.toString("utf8"));
+    }
+  });
+
+program
+  .command('list-att <msgFilePath>')
+  .description('Parse msg file and list attachment file names')
+  .action((msgFilePath) => {
+    const msgFileBuffer = fs.readFileSync(msgFilePath)
+    const testMsg = new MsgReader(msgFileBuffer)
+    const testMsgInfo = testMsg.getFileData()
+
+    for (const att of testMsgInfo.attachments) {
+      console.log(att.fileName);
+    }
+  });
+
+program
+  .command('save-att <msgFilePath> <saveToDir>')
+  .description('Parse msg file and write all attachment files')
+  .action((msgFilePath, saveToDir) => {
+    const msgFileBuffer = fs.readFileSync(msgFilePath)
+    const testMsg = new MsgReader(msgFileBuffer)
+    const testMsgInfo = testMsg.getFileData()
+
+    fs.mkdirSync(path.resolve(saveToDir), { recursive: true })
+
+    for (const att of testMsgInfo.attachments) {
+      const attFilePath = path.resolve(saveToDir, att.fileName);
+      fs.writeFileSync(attFilePath, testMsg.getAttachment(att).content)
+    }
   });
 
 program
