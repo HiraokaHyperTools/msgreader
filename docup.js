022fa4e23aaefc85088ae1bfae882ef9ffc14190
kenjiuno <ku@digitaldolphins.jp>
2021-10-15 13:45:32 +0900

    - Adding docup.js

---
+++ docup.js
@@ -0,0 +1,16 @@
+// for package maintenance: document auto updater.
+// usage: node docup.js
+
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function run() {
+  await exec("yarn typedoc", {});
+  await exec("git commit -a -m \"Update doc\"", { cwd: "docs" });
+  await exec("git push", { cwd: "docs" });
+  await exec("git commit docs -m \"- Update doc\"", {});
+  await exec("git push", {});
+  console.info("OK!");
+}
+
+run();
