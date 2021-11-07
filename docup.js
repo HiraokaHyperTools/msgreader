8cebfb4df1853e9ae37a8140e297a5af8fe8cbae
kenjiuno <ku@digitaldolphins.jp>
2021-11-07 11:50:46 +0900

    - Update docup

--- docup.js
+++ docup.js
@@ -6,6 +6,7 @@ const exec = util.promisify(require('child_process').exec);
 
 async function run() {
   await exec("yarn typedoc", {});
+  await exec("git add -A", { cwd: "docs" });
   await exec("git commit -a -m \"Update doc\"", { cwd: "docs" });
   await exec("git push", { cwd: "docs" });
   await exec("git commit docs -m \"- Update doc\"", {});
